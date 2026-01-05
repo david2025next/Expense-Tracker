65333b6d9650bc29bc465ed313c901546cc2c9bb
Roman Elizarov <davidayeledoux@gmail.com>
2026-01-05 14:19:57 +0100

    style : ajout des champs d'entree pour le montant et la description

---
+++ app/src/main/java/com/example/expensetracker/AddExpenseScreen.kt
@@ -0,0 +1,86 @@
+package com.example.expensetracker
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Notes
+import androidx.compose.material.icons.filled.AttachMoney
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+
+@Composable
+private fun InputField(
+    value: String,
+    icon: ImageVector,
+    label: String,
+    isNumber: Boolean,
+    onValueChange: (String) -> Unit
+) {
+    Column(
+
+    ) {
+        Text("$label ${if(isNumber) "" else " (optionel)"}")
+        OutlinedTextField(
+            value = value,
+            onValueChange = onValueChange,
+            leadingIcon = {
+                Icon(
+                    imageVector = icon,
+                    contentDescription = label
+                )
+            },
+            singleLine = if(isNumber) true else false,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = if (isNumber) KeyboardType.Number else KeyboardType.Text
+            ),
+            modifier = Modifier.fillMaxWidth(),
+            suffix = {
+                if (isNumber) Text("FCFA")
+                else null
+            },
+            shape = RoundedCornerShape(12.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun InputFieldNumberPreview(){
+    var amount by remember { mutableIntStateOf(0) }
+    InputField(
+        value = amount.toString(),
+        isNumber = true,
+        label = "Amount",
+        icon = Icons.Default.AttachMoney
+    ) { amount = it.toInt()}
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun InputFieldDescriptionPreview(){
+    var description by remember { mutableStateOf("") }
+    InputField(
+        value = description,
+        isNumber = false,
+        label = "Description",
+        icon = Icons.AutoMirrored.Filled.Notes
+    ) {description = it }
+}
+
+
