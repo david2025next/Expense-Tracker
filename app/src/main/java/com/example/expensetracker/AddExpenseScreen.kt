d16b7d674954460724baa50338d65eaeb183074d
Roman Elizarov <davidayeledoux@gmail.com>
2026-01-05 15:57:01 +0100

    style : ajout du champ de date

--- app/src/main/java/com/example/expensetracker/AddExpenseScreen.kt
+++ app/src/main/java/com/example/expensetracker/AddExpenseScreen.kt
@@ -1,5 +1,8 @@
 package com.example.expensetracker
 
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,12 +11,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Notes
 import androidx.compose.material.icons.filled.AttachMoney
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -21,21 +28,88 @@ import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.expensetracker.utils.categoriesMenu
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.util.Date
+import java.util.Locale
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun DateTransaction(selectedDate: Long, onDateSelected: (Long) -> Unit) {
+
+    var showModal by remember { mutableStateOf(false) }
+    val datePickerState = rememberDatePickerState()
+    Column() {
+        Text(text = "Date")
+        OutlinedTextField(
+            value = convertMillisToDate(selectedDate),
+            onValueChange = {},
+            shape = RoundedCornerShape(16.dp),
+            readOnly = true,
+            trailingIcon = {
+                Icon(Icons.Default.DateRange, contentDescription = "Select date")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .pointerInput(selectedDate) {
+                    awaitEachGesture {
+                        awaitFirstDown(pass = PointerEventPass.Initial)
+                        val upEvent = waitForUpOrCancellation(pass = PointerEventPass.Initial)
+                        if (upEvent != null) {
+                            showModal = true
+                        }
+                    }
+                }
+        )
+
+    }
+    if (showModal) {
+        DatePickerDialog(
+            onDismissRequest = { showModal = false },
+            confirmButton = {
+                TextButton(onClick = {
+                    datePickerState.selectedDateMillis?.let { onDateSelected(it) }
+                    showModal = false
+                }) {
+                    Text("OK")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { showModal = false }) {
+                    Text("Cancel")
+                }
+            }
+        ) {
+            DatePicker(state = datePickerState)
+        }
+    }
+}
+
+fun convertMillisToDate(millis: Long): String {
+    val formatter = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
+    return formatter.format(Date(millis))
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -57,7 +131,7 @@ private fun CategoryMenu() {
             modifier = Modifier
                 .fillMaxWidth()
                 .menuAnchor(),
-            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)}
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) }
         )
         ExposedDropdownMenu(
             expanded = expanded,
@@ -69,7 +143,7 @@ private fun CategoryMenu() {
                         Row(
                             modifier = Modifier.fillMaxWidth(),
                             verticalAlignment = Alignment.CenterVertically
-                        ){
+                        ) {
                             Icon(
                                 imageVector = category.icon,
                                 contentDescription = category.name,
@@ -153,14 +227,36 @@ private fun InputFieldDescriptionPreview() {
 
 @Preview(showBackground = true)
 @Composable
-private fun CategoryMenuPreview(){
+private fun CategoryMenuPreview() {
     Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
 
-    ) {
+        ) {
         CategoryMenu()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DateTransactionPreview() {
+    var date by remember {
+        mutableLongStateOf(
+            LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli()
+        )
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+        DateTransaction(date) { date = it}
     }
 }
 
